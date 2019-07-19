999dbca31391dc7c1cbb96115b08ba2878238780
Farzad Abdolhosseini <farzad.abdolhosseini@gmail.com>
2019-07-18 18:22:22 -0700

    Phase plot and its metric added to evaluation

--- playground/evaluate.py
+++ playground/evaluate.py
@@ -13,7 +13,7 @@ import torch
 from common.envs_utils import make_env
 from common.render_utils import StatsVisualizer
 from common.sacred_utils import ex
-from symmetry.metric_utils import MetricsEnv
+from symmetry.metric_utils import MetricsEnv, PhasePlotEnv
 
 import symmetry.sym_envs
 from symmetry.env_utils import get_env_name_for_method
@@ -23,11 +23,24 @@ from symmetry.env_utils import get_env_name_for_method
 def config():
     net = None
     render = False
-    max_steps = 5000
+    max_steps = 10000
     env_name = ""
     experiment_dir = "."
     assert experiment_dir != "."
     ex.add_config(os.path.join(experiment_dir, "configs.json"))  # loads saved configs
+
+
+def get_main_joint_name(env_name):
+    if "Cassie" in env_name:  # TODO: doesn't work
+        return "hip_flexion"
+    elif "Walker2D" in env_name:
+        return "thigh"
+    elif "Walker3D" in env_name:
+        return "hip_y"
+    else:
+        raise ValueError(
+            "Environment %s not supported in evaluation. Please use Walker2D, Walker3D, or Cassie."
+        )
 
 
 @ex.automain
@@ -42,16 +55,19 @@ def main(_config):
         args.experiment_dir, "models", "{}_best.pt".format(env_name)
     )
 
-    env = make_env(env_name, render=args.render)
-
-    # TODO: assert Walker3D or Bullet
-    env = MetricsEnv(env)
-    env.seed(1093)
-
     print("Env: {}".format(env_name))
     print("Model: {}".format(os.path.basename(model_path)))
 
     actor_critic = torch.load(model_path)
+
+    env = make_env(env_name, render=args.render)
+
+    # TODO: assert Walker3D or Bullet
+    env = MetricsEnv(env)
+    env = PhasePlotEnv(
+        env, get_main_joint_name(env_name), args.experiment_dir, dt=1 / 240
+    )
+    env.seed(1093)
 
     ep_reward = 0
 
@@ -72,7 +88,7 @@ def main(_config):
 
         obs, reward, done, info = env.step(cpu_actions)
 
-        if "Bullet" in env_name:
+        if "Bullet" in args.env_name:
             env.unwrapped._p.resetDebugVisualizerCamera(
                 3, 0, -5, env.unwrapped.robot.body_xyz
             )
@@ -80,7 +96,7 @@ def main(_config):
         if "metrics" in info:
             if args.render:
                 print(info["metrics"])
-                time.sleep(1)
+                time.sleep(0.4)
             for k, v in info["metrics"].items():
                 if k not in metrics:
                     metrics[k] = []
@@ -91,8 +107,8 @@ def main(_config):
             obs = env.reset()
 
     for k, v in metrics.items():
-        metrics[k] = torch.FloatTensor(v).median().item()
-        print("%s SI: %5.1f" % (k, metrics[k]))
+        metrics[k] = torch.FloatTensor(v).mean().item()
+        print("%s SI: %6.2f" % (k, metrics[k]))
 
     with open(os.path.join(args.experiment_dir, "evaluate.json"), "w") as eval_file:
         json.dump(metrics, eval_file)
--- symmetry/metric_utils.py
+++ symmetry/metric_utils.py
@@ -3,8 +3,13 @@ import numpy as np
 import copy
 import os
 import gym
+import gym.wrappers
 import gym.spaces
 import gym.envs
+
+from scipy.ndimage.filters import gaussian_filter1d
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
 
 def compute_si(values):
@@ -30,16 +35,16 @@ class MetricsEnv(gym.Wrapper):
         obs = self.env.reset(*args, **kwargs)
         com_inds = [
             i
-            for i, joint in enumerate(self.env.unwrapped.robot.ordered_joints)
+            for i, joint in enumerate(self.unwrapped.robot.ordered_joints)
             if "abdomen" in joint.joint_name
         ]
         self.left_joint_inds = [
             i
-            for i, joint in enumerate(self.env.unwrapped.robot.ordered_joints)
+            for i, joint in enumerate(self.unwrapped.robot.ordered_joints)
             if "left" in joint.joint_name
         ]
         self.right_joint_inds = list(
-            set(range(len(self.env.unwrapped.robot.ordered_joints)))
+            set(range(len(self.unwrapped.robot.ordered_joints)))
             - set(com_inds)
             - set(self.left_joint_inds)
         )
@@ -52,7 +57,7 @@ class MetricsEnv(gym.Wrapper):
         self.metrics = {"torque": [], "joint_angle": []}
 
     def integrate_metrics(self, info):
-        side_contact = self.env.unwrapped.robot.feet_contact[0]
+        side_contact = self.unwrapped.robot.feet_contact[0]
         strike = not self.prev_contact and side_contact
         self.prev_contact = side_contact
 
@@ -76,8 +81,184 @@ class MetricsEnv(gym.Wrapper):
             [action[self.left_joint_inds], action[self.right_joint_inds]]
         )
         joint_angles = np.array(
-            [j.get_position() for j in self.env.unwrapped.robot.ordered_joints]
+            [j.get_position() for j in self.unwrapped.robot.ordered_joints]
         )
         self.metrics["joint_angle"].append(
             [joint_angles[self.left_joint_inds], joint_angles[self.right_joint_inds]]
         )
+
+
+def average_values(array):
+    max_len = max([len(stride) for stride in array])
+
+    # fix the lengths (all will be stretched to `max_len`)
+    array = [
+        interp1d(range(len(arr)), arr)(np.linspace(0, len(arr) - 1, max_len))
+        for arr in array
+    ]
+
+    # smooth out the signal and average over strides
+    return (
+        gaussian_filter1d(np.concatenate(array), sigma=5)
+        .reshape((-1, max_len))
+        .sum(axis=0)
+    )
+
+
+def calc_best_distance(ql, qdotl, qr, qdotr):
+    """
+    Calculates best distance with optimal shift (to get rid of phase difference)
+    """
+    # use max to make the metric scale invariant
+    max_qdot = np.max(np.abs(np.concatenate([qdotl, qdotr])))
+    max_q = np.max(np.abs(np.concatenate([ql, qr])))
+
+    vl = np.stack([ql / max_q, qdotl / max_qdot]).transpose()
+    vr = np.stack([qr / max_q, qdotr / max_qdot]).transpose()
+
+    best_dist, best_shift = float("inf"), 0
+    for shift in range(vl.shape[0]):
+        dist = np.linalg.norm(vl - np.roll(vr, shift), ord=2, axis=-1).mean()
+        if dist < best_dist:
+            best_dist, best_shift = dist, shift
+
+    return best_dist, best_shift
+
+
+def compute_phase_plot_si(ql, qdotl, qr, qdotr, skip_strides=0):
+    """
+    Computes a symmetric index based on the phase plot (q/qdot) of left and right
+
+    Arguments:
+        ql, qdotl, qr, qdotr: array of length `num_strides` each containing another array (sizes of the inner arrays may differ)
+        skip_strides: number of initial strides to ignore
+    """
+    ql = average_values(ql[skip_strides:])
+    qdotl = average_values(qdotl[skip_strides:])
+    qr = average_values(qr[skip_strides:])
+    qdotr = average_values(qdotr[skip_strides:])
+
+    distance, shift = calc_best_distance(ql, qdotl, qr, qdotr)
+
+    return distance, ql, qdotl, qr, qdotr
+
+
+class PhasePlotEnv(gym.Wrapper):
+    def __init__(
+        self, env, joint_name, save_path, strides=10, skip_strides=2, dt=1 / 480
+    ):
+        super().__init__(env)
+        self.joint_name = joint_name
+        self.save_path = save_path
+        self.skip_strides = skip_strides
+        self.strides = strides + skip_strides
+        self.dt = dt
+
+        while isinstance(env, gym.Wrapper):
+            if isinstance(env, gym.wrappers.TimeLimit):
+                env._max_episode_steps = float("inf")
+            env = env.env
+
+        # TODO assert PyBullet or similar
+
+    def reset(self, *args, **kwargs):
+        obs = self.env.reset(*args, **kwargs)
+        self.unwrapped._p.setPhysicsEngineParameter(fixedTimeStep=self.dt)
+        joints = [
+            joint
+            for joint in self.unwrapped.robot.ordered_joints
+            if self.joint_name in joint.joint_name
+        ]
+        assert len(joints) == 2
+        if "left" in joints[0].joint_name:
+            self.left_joint, self.right_joint = joints
+        else:
+            self.right_joint, self.left_joint = joints
+        self.prev_contact = True
+        self.strike_num = 0
+        self.steps_since = 0
+        self.reset_metrics()
+        return obs
+
+    def reset_metrics(self):
+        self.ql = [[] for _ in range(self.strides)]
+        self.qdotl = [[] for _ in range(self.strides)]
+        self.qr = [[] for _ in range(self.strides)]
+        self.qdotr = [[] for _ in range(self.strides)]
+
+    def integrate_metrics(self, info, done):
+        side_contact = self.unwrapped.robot.feet_contact[0]
+        strike = not self.prev_contact and side_contact
+        self.prev_contact = side_contact
+        self.steps_since += 1
+
+        if strike and self.steps_since * self.dt > 0.5:
+            self.steps_since = 0
+            self.strike_num += 1
+            if self.strike_num == self.strides:
+                done = True
+
+                distance, ql, qdotl, qr, qdotr = compute_phase_plot_si(
+                    self.ql,
+                    self.qdotl,
+                    self.qr,
+                    self.qdotr,
+                    skip_strides=self.skip_strides,
+                )
+                info["metrics"] = {"phase_plot_index": distance}
+
+                plt.ioff()
+                plt.plot(ql, qdotl, "ro", markersize=1)
+                plt.plot(qr, qdotr, "go", markersize=1)
+                plt.savefig(os.path.join(self.save_path, "phase_plot.png"))
+                # plt.show()
+
+        return info, done
+
+    def step(self, action):
+        obs, rew, done, info = self.env.step(action)
+        self.compute_side_metrics(action)
+        info, done = self.integrate_metrics(info, done)
+        return obs, rew, done, info
+
+    def compute_side_metrics(self, action):
+        ql, qdotl = self.left_joint.get_state()
+        qr, qdotr = self.right_joint.get_state()
+        self.ql[self.strike_num].append(ql)
+        self.qdotl[self.strike_num].append(qdotl)
+        self.qr[self.strike_num].append(qr)
+        self.qdotr[self.strike_num].append(qdotr)
+
+
+if __name__ == "__main__":
+    import pickle
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please provide the states data path")
+        sys.exit(1)
+
+    with open(sys.argv[1], "rb") as fp:
+        state = pickle.load(fp)
+
+    clen = 1680
+    steps = 20
+
+    motor_pos = np.zeros((steps, clen, 10))
+    motor_vel = np.zeros((steps, clen, 10))
+
+    for i in range(steps):
+        for j in range(clen):
+            for k in range(10):
+                motor_pos[i][j][k] = state[i * clen + j][0][k]
+                motor_vel[i][j][k] = state[i * clen + j][1][k]
+
+    distance, ql, qdotl, qr, qdotr = compute_phase_plot_si(
+        motor_pos[:, :, 3], motor_vel[:, :, 3], motor_pos[:, :, 8], motor_vel[:, :, 8]
+    )
+    print("Distance %6.3f" % distance)
+    plt.ioff()
+    plt.plot(ql, qdotl, "ro", markersize=1)
+    plt.plot(qr, qdotr, "go", markersize=1)
+    plt.savefig(sys.argv[1] + "_phase_plot.png")
+
